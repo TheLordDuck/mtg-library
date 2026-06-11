a6e6c0e13d08eef57a96a3c1b4c2fcbe40647474
thelordduck22 <thelordduck22@gmail.com>
2026-06-11 14:41:39 +0200

    changes dockerfile

--- scripts/postbuild.mjs
+++
@@ -1,7 +0,0 @@
-import { cpSync } from "fs";
-
-// Copy public/ into standalone so collection.csv is accessible
-cpSync("public", ".next/standalone/public", { recursive: true });
-
-// Copy static assets
-cpSync(".next/static", ".next/standalone/.next/static", { recursive: true });
